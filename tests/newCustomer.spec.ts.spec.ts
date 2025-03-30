ee3ed0f5a188a62dea82fb2dc3a1b6f7851bfe75
apple <gbengaolabiyi5@gmail.com>
2025-03-30 05:10:18 +0300

    walle test 4

--- tests/newCustomer.spec.ts.spec.ts
+++ tests/newCustomer.spec.ts.spec.ts
@@ -3,9 +3,11 @@ import { test, expect } from '@playwright/test';
 test.describe('Account creation', () => {
     test('create account', async ({ page }) => {
         await page.goto('https://magento-2.showcase-wallee.com');
+        await page.locator('[name="firstname"]').click();
         await page.locator('[name="firstname"]').fill('New');
+        await page.locator('[name="lastname"]').click();
         await page.locator('[name="lastname"]').fill('User');
-        const email = `newuser_${Date.now()}@example.com`; // Unique email for testing
+        const email = `newuser_${Date.now()}gbe@example.com`;
         await page.locator('[name="email"]').fill(email);
         await page.locator('[name="password"]').fill('Password123!');
         await page.locator('[name="password_confirmation"]').fill('Password123!');
